05b2d1d7bb7f3fa08eec0d0c221a984225999539
Bilal <mohammedbilal345.com>
2025-01-27 17:27:34 +0500

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Successful"}
+    return {"mission passed"}
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8000)
